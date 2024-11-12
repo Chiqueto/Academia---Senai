06565dcc44456ab10b8e96d090a9a2e8b16ca2b7
Marilia <mariliateixeirainacio84@gmail.com>
2024-11-12 10:19:11 -0300

    10:13

--- routes/academia.js
+++ routes/academia.js
@@ -13,4 +13,23 @@ router.get("/menu", (req,res) => {
   res.render("academia/menuAcademia")
 });
 
+router.get("/perfil", (req,res) => {
+  res.render("academia/perfil")
+});
+
+router.get("/alunos", (req,res) => {
+  res.render("academia/alunos")
+});
+
+router.get("/personais", (req,res) => {
+  res.render("academia/personais")
+});
+
+router.get("/aparelho", (req,res) => {
+  res.render("academia/aparelho")
+});
+
+
+
+
 module.exports = router;
