b93a579054ddadd26cfeb29df64dc541633757ec
Edney7 <edneyaraujosilva00@gmail.com>
2024-11-12 15:34:05 -0300

    15:34

--- routes/aluno.js
+++ routes/aluno.js
@@ -8,6 +8,24 @@ router.get("/cadastro", alunoController.renderizaCadastro);
 router.get("/menu", alunoController.renderizaMenu, authMiddleware);
 router.get("/perfilAluno", alunoController.renderizaPerfil, authMiddleware)
 
+router.get("/opcaoTreinoAluno", (req,res) => {
+    res.render("aluno/opcaoTreinoAluno")
+  });
+
+  router.get("/encontrarAcademia", (req,res) => {
+    res.render("aluno/encontrarAcademia")
+  });
+
+  router.get("/TreinoA", (req,res) => {
+    res.render("aluno/TreinoA")
+  });
+  router.get("/TreinoB", (req,res) => {
+    res.render("aluno/TreinoB")
+  });
+  router.get("/Treinoc", (req,res) => {
+    res.render("aluno/Treinoc")
+  });
+
 router.post("/cadastro", alunoController.criarAluno);
 router.get("/listaAlunos", authMiddleware, alunoController.listarAlunos);
 router.get("/buscarAluno/:id", authMiddleware, alunoController.buscarAluno);
