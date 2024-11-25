a6dc129f42320a75b3809a4b72e703bd01dac296
Edney7 <edneiaraujosilva00@gmail.com>
2024-11-25 08:13:37 -0300

    08:13

--- controllers/aluno.js
+++ controllers/aluno.js
@@ -31,13 +31,13 @@ const calcularIdade = (dataNascimento) => {
 const criarAluno = async (req, res) => {
   const { nome, email, senha, genero, telefone, dt_nascimento } = req.body;
 
-  const telefoneFormatado = telefone.replace(/\D/g, "");
-  const senhaCriptografada = await bcrypt.hash(senha, 10);
-
   //validações de campos em branco
   if (!nome || !email || !senha || !genero || !telefone || !dt_nascimento) {
     return res.status(400).json({ message: "Preencha todos os campos!" });
   }
+
+  const telefoneFormatado = telefone.replace(/\D/g, "");
+  const senhaCriptografada = await bcrypt.hash(senha, 10);
 
   //verifica email
   const db_email = await Aluno.findByEmail(email);
@@ -70,22 +70,40 @@ const criarAluno = async (req, res) => {
 const listarAlunos = async (req, res) => {
   try {
     const alunos = await Aluno.findAll();
-    res.status(201).json(alunos);
+    res.status(200).json(alunos);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
+
+// const buscarAluno = async (req, res) => {
+//   const { id } = req.params;
+
+//   try {
+//     const aluno = await Aluno.findById(id);
+//     if (aluno) {
+//       res.status(200).json(aluno);
+//     } else {
+//       res.status(404).json({ message: "Aluno não encontrado" });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
 const buscarAluno = async (req, res) => {
   const { id } = req.params;
 
   try {
     const aluno = await Aluno.findById(id);
-    if (aluno) {
-      res.status(200).json(aluno);
-    } else {
-      res.status(404).json({ message: "Aluno não encontrado" });
+    if (!aluno) {
+      return res.status(404).json({ message: "Aluno não encontrado" });
     }
+
+    aluno.telefone = formatarTelefone(aluno.telefone);
+    aluno.idade = calcularIdade(aluno.dt_nascimento);
+
+    res.render("aluno/perfilAluno", { aluno });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -114,6 +132,7 @@ const atualizarAluno = async (req, res) => {
   if (!nome || !telefone) {
     return res.status(400).json({ message: "Preencha todos os campos!" });
   }
+
   const telefoneFormatado = telefone.replace(/\D/g, "");
   try {
     const result = await Aluno.updateAluno(id, { nome, telefoneFormatado });
@@ -212,14 +231,14 @@ const autenticaAluno = async (req, res) => {
       expiresIn: "1h",
     });
 
-    return res.status(200).json({
+    res.status(200).json({
       message: "Aluno autenticado com sucesso!",
       token,
       redirectTo: `/aluno/menu/${aluno.id}`,
     });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: "Erro interno do servidor" });
+    res.status(500).json({ error: "Erro interno do servidor" });
   }
 };
 
